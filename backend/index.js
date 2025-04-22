9c3b1df425523a0560ac8b6257c5d139270e1d87
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-04-22 12:36:14 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(PORT, ()=>{
+    console.log("Server is Successfully running on bilow PORT")
+    console.log(`http://localhost:${PORT}`)
+})
